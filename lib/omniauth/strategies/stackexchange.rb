2898ce04a32064cba65ec25674ea5f9d074aec1c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-11-19 10:59:18 +0000

    Add callback url to fix regression for oauth2 1.4

--- lib/omniauth/strategies/stackexchange.rb
+++ lib/omniauth/strategies/stackexchange.rb
@@ -61,6 +61,12 @@ module OmniAuth
           'stackoverflow'
       end
 
+      def callback_url
+        # Fixes regression in omniauth-oauth2 v1.4.0 by
+        # https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+        options[:callback_url] || (full_host + script_name + callback_path)
+      end
+
       def callback_phase
         super
       rescue NotRegisteredForStackExchangeSiteError => e
